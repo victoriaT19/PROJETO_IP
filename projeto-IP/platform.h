bcfa3c39e96b47b6ef50c5f9217dde151d125dbf
victoriaT19 <76068991+victoriaT19@users.noreply.github.com>
2024-03-12 19:49:49 -0300

    Update platform.h

--- projeto-IP/platform.h
+++ projeto-IP/platform.h
@@ -3,14 +3,22 @@
 
 #include "raylib.h"
 
+#define PLATFORM_SPEED_X 5
+#define PLATFORM_SPEED_Y 0
+
 #define PLATFORMS_NUMBER 1000
+
 
 typedef struct
 {
     Vector2 platformPos;
     Rectangle platformHitbox;
+    Vector2 platformSpeed;
+    bool isMoving;
 } Platform;
 
 void generatePlatforms(Platform *platforms);
+void CreateMovingPlatforms(Platform *platforms, int numPlatforms);
+void movePlatforms(Platform *platforms, int numPlatforms);
 
 #endif
